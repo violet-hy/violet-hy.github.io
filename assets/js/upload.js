4d4425d4476df71777885a11a18d3838c48810bc
BuildTools <unconfigured@null.spigotmc.org>
2024-11-29 22:39:29 -0800

    initial user upload testing with tentative backend integration

---
+++ assets/js/upload.js
@@ -0,0 +1,36 @@
+document.getElementById('upload').addEventListener('submit',async function(event){
+    event.preventDefault();
+    const imgID = document.getElementById('imgID').value;
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+
+    // const output = JSON.stringify({imgID, title, description})
+    // console.log(output)
+
+    const formData = {
+        [imgID]: {
+            title: title,
+            description: description
+        }
+    };
+
+    const endpoint = 'https://h82pxx95va.execute-api.us-west-1.amazonaws.com/main';
+
+    try{
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            },
+            body: JSON.stringify(formData)
+        })
+
+        const result = await response.JSON();
+        alert(result.message);
+    }
+    catch(error) {
+        console.error('Upload Error',error)
+    }
+    
+})
