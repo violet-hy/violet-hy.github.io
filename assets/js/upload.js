7af789d2c1744237a6cc61f6b007cfab7daa55a2
BuildTools <unconfigured@null.spigotmc.org>
2024-12-01 19:41:52 -0800

    fixed b64 encoding

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,6 +1,14 @@
 document.getElementById('upload').addEventListener('submit',async function(event){
     event.preventDefault();
 
+    // API endpoint
+    const endpoint = 'https://h82pxx95va.execute-api.us-west-1.amazonaws.com/main/uploadresource';
+    //grab the auth token from url 
+    const id_token = () => {
+        const hashParams = new URLSearchParams(window.location.hash.substring(1));
+        console.log(hashParams.get('id_token'));
+        return hashParams.get('id_token');
+    };
     
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
@@ -14,46 +22,48 @@ document.getElementById('upload').addEventListener('submit',async function(event
         }
     
     const imgData = document.getElementById('imgFile').files[0];
+    let imgDataB64 = "";
+
     if (imgData) {
         const reader = new FileReader();
+
         reader.onloadend = () =>{
-            const imgDataB64 = reader.result.split(',')[1];
+            imgDataB64 = reader.result.split(',')[1];
             console.log(imgDataB64)
+
+            formData.append('imgData', imgDataB64);
+            formData.append('jsonData', JSON.stringify(jsonData));
+
+            sendData(formData);
         }
         reader.readAsDataURL(imgData);
     }
-        
-    formData.append('jsonData', JSON.stringify(jsonData));
-    formData.append('imgData', imgDataB64);
-
-    //console.log(formData);
-
-
-    // API endpoint
-    const endpoint = 'https://h82pxx95va.execute-api.us-west-1.amazonaws.com/main/uploadresource';
-    //grab the auth token from url 
-    const id_token = () => {
-        const hashParams = new URLSearchParams(window.location.hash.substring(1));
-        console.log(hashParams.get('id_token'));
-        return hashParams.get('id_token');
-    };
-
+    
     // send a POST request to our API containing our image and description
-    try{
-        const response = await fetch(endpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'Authorization': id_token()
-            },
-            body: formData
-        });
+    async function sendData(formData) { 
+        
+        try{
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': id_token()
+                },
+                body: formData
+            });
+    
+            const result = await response.json();
+            alert(result.message);
+        }
+        catch(error) {
+            console.error('Upload Error',error)
+        }
+    }
+        
+    
 
-        const result = await response.json();
-        alert(result.message);
-    }
-    catch(error) {
-        console.error('Upload Error',error)
-    }
+
+
+    
     
 })
