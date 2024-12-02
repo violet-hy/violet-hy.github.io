1dd392f9cf3507ae61cf5af9bc894cbbd8e46fe9
BuildTools <unconfigured@null.spigotmc.org>
2024-12-01 18:21:52 -0800

    adding frontend image upload support

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,34 +1,44 @@
 document.getElementById('upload').addEventListener('submit',async function(event){
     event.preventDefault();
 
-    const imgID = document.getElementById('imgID').value;
+    
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
 
+    
+    const formData = new FormData();
+
+    const jsonData = {
+            title: title,
+            description: description
+        }
+    
+    const imgData = document.getElementById('imgFile').files[0];
+        
+    formData.append('jsonData', JSON.stringify(jsonData));
+    formData.append('imgData', imgData);
+
+    //console.log(formData);
+
+
+    // API endpoint
+    const endpoint = 'https://h82pxx95va.execute-api.us-west-1.amazonaws.com/main/uploadresource';
+    //grab the auth token from url 
     const id_token = () => {
         const hashParams = new URLSearchParams(window.location.hash.substring(1));
         console.log(hashParams.get('id_token'));
         return hashParams.get('id_token');
     };
 
-
-    const formData = {
-        [imgID]: {
-            title: title,
-            description: description
-        }
-    };
-
-    const endpoint = 'https://h82pxx95va.execute-api.us-west-1.amazonaws.com/main/uploadresource';
-
+    // send a POST request to our API containing our image and description
     try{
         const response = await fetch(endpoint, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'multipart/form-data',
                 'Authorization': id_token()
             },
-            body: JSON.stringify(formData)
+            body: formData
         });
 
         const result = await response.json();
