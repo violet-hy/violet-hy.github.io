ce244da45fb477013be29fe321369dce4629eae2
BuildTools <unconfigured@null.spigotmc.org>
2024-12-02 13:17:36 -0800

    added an info popover to upload page

---
+++ assets/js/popover.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    popoverTriggerList.forEach(function (popoverTriggerEl) {
+        new bootstrap.Popover(popoverTriggerEl);
+    });
+    
+});
