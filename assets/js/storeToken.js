8de2170d66954cfde91d07b86e2f9f12d726ae6c
BuildTools <unconfigured@null.spigotmc.org>
2024-12-03 19:41:36 -0800

    fixed syntax error

--- assets/js/storeToken.js
+++ assets/js/storeToken.js
@@ -3,7 +3,7 @@ function checkAndStoreIdToken() {
     const idToken = urlParams.get('id_token') || (window.location.hash && window.location.hash.includes('id_token') ? new URLSearchParams(window.location.hash.substring(1)).get('id_token') : null);
 
     if (idToken) {
-        sessionStorageStorage.setItem('id_token', idToken);
+        sessionStorage.setItem('id_token', idToken);
         console.log(idToken);
     }
 }
