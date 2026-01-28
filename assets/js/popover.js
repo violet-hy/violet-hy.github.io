17117cfd882643100ea294b84e112d4a7b3c0be9
Violet Harley <violet.j.harley@lookxsee.com>
2026-01-28 12:58:11 -0800

    refreshing for new branch

--- assets/js/popover.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-    popoverTriggerList.forEach(function (popoverTriggerEl) {
-        new bootstrap.Popover(popoverTriggerEl);
-    });
-    
-});
